66d5af0cdf0d089024b972fd770e5c831ba8c4dc
rodrigo <rob.developer@protonmail.com>
2021-08-01 14:40:44 -0400

    convert data in ufs model objects in db

--- app/models/uf.rb
+++ app/models/uf.rb
@@ -1,2 +1,20 @@
 class Uf < ApplicationRecord
+    def self.csv_uf_file
+
+        #guardar en una variable un objeto de tipo File
+        file = File.open('tmp/UF 2019.csv') 
+        #quitar salto de línea y guardar array en una variable
+        data = file.readlines.map(&:chomp)
+        #depurar data y convertirlos a objetos del modelo
+        data.each_with_index do |row, index|
+            next if index.zero?
+            col = row.split(",")
+            #crear valores para enero
+            Uf.create(date: "2019/01/#{col[0]}", uf_value: col[1])
+            #crear valores para febrero
+            if index < 29
+                Uf.create(date: "2019/02/#{col[0]}", uf_value: col[2])        
+            end
+        end
+    end
 end
